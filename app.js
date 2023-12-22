9cd8dc77cbec4b27ee21467807be4e1694515f18
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 17:20:26 +0330

    Add middleware to fetch and attach user data to the request object.

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const data = require("./database/database").connectDb
+const User = require("./models/User")
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use( async (req,res,next)=>{
+    try{
+        const user = await User.findById('658592d76ef84dc1adf01bc1')
+        if (!user) {
+            return res.redirect('/')
+        }
+        req.user = new User(user.name , user.email)
+        next()
+    }catch(err){
+        console.log(err);
+    }
+})
 const homeRoutes = require("./routes/home");
 const shopRoutes = require('./routes/shop')
 const adminRoute = require("./routes/admin")
