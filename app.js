c82070b4ab365d95b7fd3dab23908556483fb683
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 12:09:49 +0330

    Added EJS view engine, body-parser, and static file serving.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
-
+const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({extended : false}))
 
+app.set('view engine' , 'ejs')
+app.set('views', 'views')
+app.use(express.static(path.join(__dirname , "public")))
 
 app.listen(3000,()=>{
     console.log('run server on port 3000');
