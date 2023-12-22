0085262240608239c4242a2519a3ab0f51eed0da
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 18:25:24 +0330

    Update findOne() method in User model to use the correct collection name " Users" (with a space) instead of "Users".

--- models/User.js
+++ models/User.js
@@ -15,8 +15,7 @@ class User {
     static async findById(id){
         console.log('id =>',id);
         const db = getDb()
-        const user = await db.collection("Users").findOne({_id: new objectId(id) })
-        // const user = await db.collection(" Users").findOne({ _id: new objectId(id) });
+        const user = await db.collection(" Users").findOne({ _id: new objectId(id) })
     console.log('user =>',user);
     }
 }
