f9631eba6b9dc17aa1314e5ec5c195eb3e75c29a
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 18:24:32 +0330

    Update User model to use MongoDB's ObjectId class for the _id field.

--- models/User.js
+++ models/User.js
@@ -1,29 +1,24 @@
-const mongodb = require("mongodb");
-const getDb = require("../database/database").getDb;
-const objectId = mongodb.ObjectId;
+const mongodb = require('mongodb')
+const getDb = require("../database/database").getDb
+const objectId = mongodb.ObjectId
+
 
 class User {
-  constructor(username, email) {
-    this.username = username;
-    this.email = email;
-  }
-  async save() {
-    const db = getDb();
-    return db.collection("Users").insertOne(this);
-  }
-  static async findById(id) {
-    console.log("id =>", id);
-    const db = getDb();
-    try {
-      const newId = new objectId("65859b466ef84dc1adf01bc2");
-      const user = await db.collection("Users").findOne({ _id: newId });
-      console.log("user=>", user);
-      return user;
-    } catch (err) {
-      console.error('Error finding user by ID:', err);
-      throw err; 
+    constructor(username , email){
+        this.username = username
+        this.email = email
     }
-  }
+    async save(){
+        const db = getDb()
+        return db.collection("Users").insertOne(this)
+    }
+    static async findById(id){
+        console.log('id =>',id);
+        const db = getDb()
+        const user = await db.collection("Users").findOne({_id: new objectId(id) })
+        // const user = await db.collection(" Users").findOne({ _id: new objectId(id) });
+    console.log('user =>',user);
+    }
 }
 
-module.exports = User;
+module.exports = User
