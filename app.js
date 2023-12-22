8dfca6050f8834e77f48dfd615af619c6ded3428
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 12:16:45 +0330

    Added EJS view engine, static assets serving, and home routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.urlencoded({extended : false}))
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine' , 'ejs')
-app.set('views', 'views')
-app.use(express.static(path.join(__dirname , "public")))
+const homeRoutes = require("./routes/home");
+app.use(homeRoutes);
 
-app.listen(3000,()=>{
-    console.log('run server on port 3000');
-})
+app.listen(3000, () => {
+  console.log("run server on port 3000");
+});
