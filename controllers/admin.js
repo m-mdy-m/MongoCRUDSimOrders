9c50b4cb1d0a7cc322ef6443dee9eea5b3fcfab9
mahdi <mahdimamashli1383@gmail.com>
2023-12-22 14:09:50 +0330

    Add products to the admin dashboard

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,9 @@
-exports.getDashboard = (req, res) => {
-  res.render("admin/dashboard", { title: "dashboard", path: req.path });
+const Product = require("../models/Product");
+exports.getDashboard = async (req, res) => {
+  const products = await Product.fetchAll();
+  res.render("admin/dashboard", {
+    title: "dashboard",
+    path: req.path,
+    products,
+  });
 };
