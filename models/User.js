a50e1ccb8fb52456948e039d862154236c59468e
mahdi <mahdimamashli1383@gmail.com>
2023-12-23 09:54:50 +0330

    Update the code to fetch products from the database and add quantity information to each product in the cart.

--- models/User.js
+++ models/User.js
@@ -49,13 +49,14 @@ class User {
   }
   async getCart (){
     const db = getDb()
-    const carts = this.cart.Selected_Products
-    const prodId = carts.map(prod =>{
-      return prod.prodId
+    const carts = this.cart.Selected_Products;
+    const prodId = carts.map(cart =>{
+      return cart.prodId
     })
-    const products = await db.collection("Product").find({_id : {$in : prodId}})
+    const products = await db.collection('Product').find({_id : {$in : prodId}}).toArray()
     return products.map(product =>{
-      console.log('pro =>', product);
+      const qty = carts.find( p => p.prodId.toString() === product._id.toString()).qty
+      return {...product, qty:qty}
     })
   }
 }
